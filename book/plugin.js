b5b00e0be6ed28078ac7620b9645bb66436a212c
billryan <yuanbin2014@gmail.com>
2015-06-25 18:55:39 +0800

    remove page.change event

--- book/plugin.js
+++ book/plugin.js
@@ -2,12 +2,4 @@ require(["gitbook"], function(gitbook) {
     gitbook.events.bind("start", function(e, config) {
         config.swiftype = config.swiftype || {};
     });
-
-    gitbook.events.bind("page.change", function() {
-        swiftype('send', 'pageview', window.location.pathname+window.location.search);
-    });
-
-    gitbook.events.bind("exercise.submit", function(e, data) {
-
-    });
 });
