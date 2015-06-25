b7cceef984a6f7aadc3d8b0bce5d9176b6a3e9aa
billryan <yuanbin2014@gmail.com>
2015-06-25 16:50:10 +0800

    remove extra configurations

--- index.js
+++ index.js
@@ -6,28 +6,21 @@ module.exports = {
         ],
         html: {
             "body:end": function() {
-                var config = this.options.pluginsConfig.ga || {};
+                var config = this.options.pluginsConfig.swiftype || {};
                 
                 if (!config.token) {
                 	throw "Need to option 'token' for Switype plugin";
                 }
                 
-                if (!config.configuration) {
-                	config.configuration = '2.0.0';
-                }
-
-                if(typeof config.configuration === 'object' && config.configuration !== null) {
-                	configuration = JSON.stringify(config.configuration);
-                }
-                else if (['2.0.0', 'none'].indexOf(config.configuration) != -1) {
-                	configuration = "'" + config.configuration + "'";
+                if (!config.version) {
+                	config.version = '2.0.0';
                 }
 
                 return "<script>(function(w,d,t,u,n,s,e){w['SwiftypeObject']=n;w[n]=w[n]||function(){}})"
                 + "(w[n].q=w[n].q||[]).push(arguments);};s=d.createElement(t);"
                 + "e=d.getElementsByTagName(t)[0];s.async=1;s.src=u;e.parentNode.insertBefore(s,e);"
                 + "})(window,document,'script','//s.swiftypecdn.com/install/v2/st.js','_st');"
-                + "_st('install', '"+config.token+"', "+configuration+");"
+                + "_st('install', '"+config.token+"', "+version+");"
                 + "</script>";
             }
         }
