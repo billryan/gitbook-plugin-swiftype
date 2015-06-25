4a518708758babe5d991e244387b2a8bde1982c7
billryan <yuanbin2014@gmail.com>
2015-06-25 23:08:03 +0800

    handle page.change event

--- book/plugin.js
+++ book/plugin.js
@@ -2,4 +2,12 @@ require(["gitbook"], function(gitbook) {
     gitbook.events.bind("start", function(e, config) {
         config.swiftype = config.swiftype || {};
     });
+
+    gitbook.events.bind("page.change", function() {
+
+    });
+
+    gitbook.events.bind("exercise.submit", function(e, data) {
+
+    });
 });
